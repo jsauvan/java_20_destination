67d5cb4c2173b724f9904fc85a38bbf2a9ec591f
SAUVAN <jerome.sauvan@capgemini.com>
2025-04-25 10:17:43 +0200

    add new feature RMI removed in java 17

---
+++ HelloImpl.java
@@ -0,0 +1,34 @@
+import java.rmi.RemoteException;
+import java.rmi.activation.Activatable;
+import java.rmi.activation.ActivationID;
+import java.rmi.activation.ActivationGroup;
+import java.rmi.activation.ActivationGroupDesc;
+import java.rmi.activation.ActivationGroupDesc.CommandEnvironment;
+import java.rmi.activation.ActivationGroupID;
+import java.rmi.activation.ActivationDesc;
+import java.util.Properties;
+
+public class HelloImpl extends Activatable implements Hello {
+    public HelloImpl(ActivationID id, MarshalledObject<?> data) throws RemoteException {
+        super(id, 0);
+    }
+
+    @Override
+    public String sayHello() throws RemoteException {
+        return "Hello, world!";
+    }
+
+    public static void main(String[] args) {
+        try {
+            Properties props = new Properties();
+            ActivationGroupDesc groupDesc = new ActivationGroupDesc(props, null);
+            ActivationGroupID groupID = ActivationGroup.getSystem().registerGroup(groupDesc);
+            ActivationDesc desc = new ActivationDesc(groupID, "HelloImpl", null, null);
+            HelloImpl obj = (HelloImpl) Activatable.register(desc);
+            System.out.println("Object registered: " + obj.sayHello());
+        } catch (Exception e) {
+            System.err.println("Exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Server.java
@@ -0,0 +1,26 @@
+import java.rmi.activation.ActivationGroup;
+import java.rmi.activation.ActivationGroupDesc;
+import java.rmi.activation.ActivationGroupID;
+import java.rmi.activation.ActivationDesc;
+import java.rmi.activation.Activatable;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Properties;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            Properties props = new Properties();
+            ActivationGroupDesc groupDesc = new ActivationGroupDesc(props, null);
+            ActivationGroupID groupID = ActivationGroup.getSystem().registerGroup(groupDesc);
+            ActivationDesc desc = new ActivationDesc(groupID, "HelloImpl", null, null);
+            HelloImpl obj = (HelloImpl) Activatable.register(desc);
+            Registry registry = LocateRegistry.createRegistry(1099);
+            registry.rebind("Hello", obj);
+            System.out.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
